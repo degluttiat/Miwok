46e90724e619173c7e9c856a91c76aca6854f8c5
degluttiat <alena.liahov@gmail.com>
2018-05-01 02:36:25 +0300

    Fragments

---
+++ app/src/main/java/com/example/android/miwok/ColorsFragment.java
@@ -0,0 +1,63 @@
+package com.example.android.miwok;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ColorsFragment extends Fragment {
+    private WordAdapter mAdapter;
+    AudioManager mAudioManager;
+
+    public ColorsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("green","chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown","ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray","ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black","kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white","kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("dusty yellow","ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("mustard yellow","chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+
+        mAdapter = new WordAdapter(getActivity(), words, R.color.category_colors, mAudioManager);
+
+        ListView listView = rootView.findViewById(R.id.list);
+
+        listView.setAdapter(mAdapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAdapter != null) {
+            mAdapter.releaseMediaPlayer();
+        }
+    }
+}
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -10,42 +10,15 @@ import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
-    private WordAdapter mAdapter;
-    AudioManager mAudioManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?","tinnә oyaase'nә", R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...","oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?","michәksәs?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.","kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?","әәnәs'aa?", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.","hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.","әәnәm", R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.","yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.","әnni'nem", R.raw.phrase_come_here));
-
-        mAdapter = new WordAdapter(this, words, R.color.category_phrases, mAudioManager);
-
-        ListView listView = findViewById(R.id.list);
-
-        listView.setAdapter(mAdapter);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mAdapter != null) {
-            mAdapter.releaseMediaPlayer();
-        }
-    }
+
 }
